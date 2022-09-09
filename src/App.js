1b027c848eac0a78caedada435f316fc91435e8c
Salokya <kumar.salokya007@gmail.com>
2022-09-09 15:49:18 +0530

    added chart

--- src/App.js
+++ src/App.js
@@ -22,23 +22,6 @@ function App() {
           <Routes>
             <Route path="/">
               <Route index element={<Home handle={handle} />} />
-              <Route path="login" element={<Login />} />
-              <Route path="users">
-                <Route index element={<List />} />
-                <Route path=":userId" element={<Single />} />
-                {/* <Route
-                path="new"
-                element={<New inputs={userInputs} title="Add New User" />}
-              /> */}
-              </Route>
-              <Route path="products">
-                <Route index element={<List />} />
-                <Route path=":productId" element={<Single />} />
-                {/* <Route
-                path="new"
-                element={<New inputs={productInputs} title="Add New Product" />}
-              /> */}
-              </Route>
             </Route>
           </Routes>
         </BrowserRouter>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,3 +1,5 @@
+import CustomBarChart from '../../components/chart/CustomBarChart'
+import CustomPieChart from '../../components/chart/CustomPieChart'
 import Navbar from '../../components/navbar/Navbar'
 import Sidebar from '../../components/sidebar/Sidebar'
 import Widget from '../../components/widgets/Widget'
@@ -10,10 +12,14 @@ function Home(props) {
       <div className="home-container">
         <Navbar handle={props.handle} />
         <div className="widgets">
-          <Widget type="user"/>
-          <Widget type="order"/>
-          <Widget type="earning"/>
-          <Widget type="balance"/>
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earning" />
+          <Widget type="balance" />
+        </div>
+        <div className="chart">
+          <CustomBarChart />
+          <CustomPieChart />
         </div>
       </div>
     </div>

1cfc242b6adbec0b27e88abc6ca62774f6df2b8f
Salokya <kumar.salokya007@gmail.com>
2022-09-09 14:54:04 +0530

    added widgets

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
 import Navbar from '../../components/navbar/Navbar'
 import Sidebar from '../../components/sidebar/Sidebar'
+import Widget from '../../components/widgets/Widget'
 import "./home.css"
 
 function Home(props) {
@@ -9,6 +9,12 @@ function Home(props) {
       <Sidebar />
       <div className="home-container">
         <Navbar handle={props.handle} />
+        <div className="widgets">
+          <Widget type="user"/>
+          <Widget type="order"/>
+          <Widget type="earning"/>
+          <Widget type="balance"/>
+        </div>
       </div>
     </div>
   )

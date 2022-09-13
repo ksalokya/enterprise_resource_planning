c7ffa1719141c3e2e4ce13bffe05b99b92424c2d
Salokya <kumar.salokya007@gmail.com>
2022-09-13 13:00:44 +0530

    added system health section and Loader

---
+++ src/components/chart/MonitorChart.jsx
@@ -0,0 +1,55 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import './chart.css'
+
+function MonitorChart() {
+
+    const [data, setData] = useState([{
+        cpu: 4000,
+        mem: 2400,
+        net: 2400,
+        disk: 2000
+    },]);
+
+    useEffect(() => {
+        setInterval(() => {
+            let a = Math.floor(Math.random() * (1500 - 1000 + 1) + 1000)
+            let b = Math.floor(Math.random() * (1000 - 800 + 1) + 800)
+            let c = Math.floor(Math.random() * (800 - 500 + 1) + 500)
+            let d = Math.floor(Math.random() * (500 - 300 + 1) + 300)
+
+            setData([{ cpu: a, mem: b, net: c, disk: d }, { cpu: 1.2 * a, mem: 1.5 * b, net: 0.8 * c, disk: 0.5 * d },
+            { cpu: 1.5 * a, mem: 0.8 * b, net: 0.5 * c, disk: 2 * d }, { cpu: a, mem: 1.3 * b, net: 0.5 * c, disk: 0.8 * d },
+            { cpu: 1.2 * a, mem: 0.5 * b, net: 0.6 * c, disk: d }, { cpu: 1.5 * a, mem: 1.1 * b, net: 0.7 * c, disk: 0.4 * d }])
+        }, 4000)
+    }, [])
+
+    return (
+        <div className="area-chart">
+            <ResponsiveContainer width="100%" height="100%">
+                <AreaChart
+                    width={500}
+                    height={400}
+                    data={data}
+                    margin={{
+                        top: 10,
+                        right: 30,
+                        left: 0,
+                        bottom: 0,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <YAxis />
+                    <Tooltip />
+                    <Area type="monotone" dataKey="cpu" stackId="1" stroke="rgb(30, 183, 255)" fill="rgb(30, 183, 255)" />
+                    <Area type="monotone" dataKey="mem" stackId="1" stroke="rgb(202, 142, 255)" fill="rgb(202, 142, 255)" />
+                    <Area type="monotone" dataKey="net" stackId="1" stroke="rgb(27, 185, 52)" fill="rgb(27, 185, 52)" />
+                    <Area type="monotone" dataKey="disk" stackId="1" stroke="rgb(247, 191, 71)" fill="rgb(247, 191, 71)" />
+                </AreaChart>
+            </ResponsiveContainer>
+        </div>
+    )
+}
+
+export default MonitorChart
--- src/pages/health/Health.jsx
+++ src/pages/health/Health.jsx
@@ -1,7 +1,7 @@
 import Navbar from '../../components/navbar/Navbar'
 import Sidebar from '../../components/sidebar/Sidebar'
 import Monitor from '../../components/monitor/Monitor'
-
+import MonitorChart from '../../components/chart/MonitorChart'
 import './health.css'
 
 function Health(props) {
@@ -10,27 +10,32 @@ function Health(props) {
       <Sidebar />
       <div className="health-container">
         <Navbar handle={props.handle} />
-        <div className="health-progress">
-          <Monitor
-            title="CPU"
-            unit="GHz"
-            color='rgb(30, 183, 255)'
-          />
-          <Monitor
-            title="Memory"
-            unit="GB"
-            color='rgb(202, 142, 255)'
-          />
-          <Monitor
-            title="Traffic"
-            unit="Mb"
-            color='rgb(27, 185, 52)'
-          />
-          <Monitor
-            title="Disk I/O"
-            unit="MB"
-            color='rgb(247, 191, 71)'
-          />
+        <div className="health-column">
+          <div className="health-progress">
+            <Monitor
+              title="CPU"
+              unit="GHz"
+              color='rgb(30, 183, 255)'
+            />
+            <Monitor
+              title="Memory"
+              unit="GB"
+              color='rgb(202, 142, 255)'
+            />
+            <Monitor
+              title="Traffic"
+              unit="Mb"
+              color='rgb(27, 185, 52)'
+            />
+            <Monitor
+              title="Disk I/O"
+              unit="MB"
+              color='rgb(247, 191, 71)'
+            />
+          </div>
+          <div className="health-chart">
+            <MonitorChart />
+          </div>
         </div>
       </div>
     </div>

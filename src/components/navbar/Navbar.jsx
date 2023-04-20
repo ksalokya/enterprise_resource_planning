43324cfa23689cfeaa1d15a2194d6a34d28a83ef
Salokya <kumar.salokya007@gmail.com>
2023-04-21 00:28:39 +0530

    UI Changes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -21,63 +21,64 @@ import NotificationsIcon from '@mui/icons-material/Notifications';
 import MoreIcon from '@mui/icons-material/MoreVert';
 import { useNavigate } from 'react-router-dom';
 
-const Search = styled('div')(({ theme }) => ({
-    position: 'absolute',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    '&:hover': {
-        backgroundColor: alpha(theme.palette.common.white, 0.25),
-    },
-    width: '100%',
-    [theme.breakpoints.only('xs')]: {
-        marginLeft: theme.spacing(2),
-        width: '50%',
-    },
-    [theme.breakpoints.up('sm')]: {
-        marginLeft: theme.spacing(3),
-        width: 'auto',
-    },
-    borderStyle: "solid",
-    borderColor: "#555",
-    top: "50%",
-    left: "45%",
-    transform: "translate(-50%, -50%)"
-}));
-
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: 'inherit',
-    '& .MuiInputBase-input': {
-        padding: theme.spacing(1, 1, 1, 0),
-        // vertical padding + font size from searchIcon
-        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-        transition: theme.transitions.create('width'),
-        width: '100%',
-        [theme.breakpoints.down("md")]: {
-            width: "30ch",
-            "&:focus": {
-                width: "32ch"
-            }
-        },
-        [theme.breakpoints.up("lg")]: {
-            width: "40ch",
-            "&:focus": {
-                width: "45ch"
-            }
-        }
-    },
-}));
 
 export default function Navbar(props) {
+    const Search = styled('div')(({ theme }) => ({
+        position: 'absolute',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: alpha(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: alpha(theme.palette.common.white, 0.25),
+        },
+        width: '100%',
+        [theme.breakpoints.only('xs')]: {
+            marginLeft: theme.spacing(2),
+            width: '50%',
+        },
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing(3),
+            width: 'auto',
+        },
+        borderStyle: "solid",
+        borderColor: "#555",
+        top: "50%",
+        left: props.open ? "45%" : "48%",
+        transform: "translate(-50%, -50%)"
+    }));
+    
+    const SearchIconWrapper = styled('div')(({ theme }) => ({
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }));
+    
+    const StyledInputBase = styled(InputBase)(({ theme }) => ({
+        color: 'inherit',
+        '& .MuiInputBase-input': {
+            padding: theme.spacing(1, 1, 1, 0),
+            // vertical padding + font size from searchIcon
+            paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+            transition: theme.transitions.create('width'),
+            width: '100%',
+            [theme.breakpoints.down("md")]: {
+                width: "30ch",
+                "&:focus": {
+                    width: "32ch"
+                }
+            },
+            [theme.breakpoints.up("lg")]: {
+                width: props.open ? "40ch" : "50ch",
+                "&:focus": {
+                    width: "45ch"
+                }
+            }
+        },
+    }));
+
     const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
 
     const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);

fbba760387c3a24b2939e87faedb23f1905b877d
Salokya <kumar.salokya007@gmail.com>
2023-04-30 15:38:20 +0530

    added kanabn header icons

--- src/components/kanban/Kanban.jsx
+++ src/components/kanban/Kanban.jsx
@@ -5,6 +5,16 @@ import Header from "../header/Header";
 import './kanban.css'
 
 function Kanban() {
+
+    function columnTemplate(props) {
+        return (
+            <div className="header-template-wrap">
+                <div className={"header-icon e-icons " + props.keyField}></div>
+                <div className="header-text">{props.headerText}</div>
+            </div>
+        );
+    }
+
     return (
         <div className="kanban-board">
             < Header title="Kanban" />
@@ -14,9 +24,13 @@ function Kanban() {
                 height="83%"
                 dataSource={kanbanData}
                 cardSettings={{ contentField: "Summary", headerField: "Id" }}
+                cssClass="kanban-header"
             >
                 <ColumnsDirective>
-                    {kanbanGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
+                    <ColumnDirective headerText="To Do" keyField="Open" template={columnTemplate.bind(this)} />
+                    <ColumnDirective headerText="In Progress" keyField="InProgress" template={columnTemplate.bind(this)} />
+                    <ColumnDirective headerText="In Review" keyField="Review" template={columnTemplate.bind(this)} />
+                    <ColumnDirective headerText="Done" keyField="Close" template={columnTemplate.bind(this)} />
                 </ColumnsDirective>
             </KanbanComponent>
         </div>

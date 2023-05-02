b3ca5800f927f1398ae76bff1e754499f37b364d
Salokya <kumar.salokya007@gmail.com>
2023-05-02 18:21:32 +0530

    frontend build files

--- client/src/components/calendar/Calendar.jsx
+++ client/src/components/calendar/Calendar.jsx
@@ -1,8 +1,7 @@
 import { ScheduleComponent, ViewsDirective, ViewDirective, Day, Week, WorkWeek, Month, Agenda, Inject, Resize, DragAndDrop } from '@syncfusion/ej2-react-schedule';
-import { DataManager, UrlAdaptor, Query } from '@syncfusion/ej2-data';
+import { scheduleData } from './data';
 import Header from "../header/Header";
 import './calendar.css'
-import { useEffect } from 'react';
 
 function Calendar() {
 
@@ -10,25 +9,14 @@ function Calendar() {
         arg.navigation.enable = true;
     }
 
-    let dataManager = new DataManager({
-        url: 'http://localhost:8080/api/v1/calendar/get',
-        crudUrl: 'http://localhost:5000/update',
-        adaptor: new UrlAdaptor(),
-        crossDomain: true
-    });
-
-    let dataQuery = new Query().addParams('email', 'user@user.com');
-    const eventSettings = { dataSource: dataManager, query: dataQuery };
-
     return (
         <div className="scheduler">
             <Header title="Calendar" />
             <ScheduleComponent
                 height="85%"
                 selectedDate={new Date(2021, 0, 10)}
-                eventSettings={eventSettings}
+                eventSettings={{ dataSource: scheduleData }}
                 dragStart={onDragStart}
-                enablePersistence={true}
             >
                 <ViewsDirective>
                     {['Day', 'Week', 'WorkWeek', 'Month', 'Agenda'].map((item) => <ViewDirective key={item} option={item} />)}
--- client/src/components/sheet/Spreadsheet.jsx
+++ client/src/components/sheet/Spreadsheet.jsx
@@ -1,55 +1,31 @@
-import { useEffect } from 'react';
-import { SpreadsheetComponent } from '@syncfusion/ej2-react-spreadsheet';
+import { SpreadsheetComponent, SheetsDirective, SheetDirective, RangesDirective } from '@syncfusion/ej2-react-spreadsheet';
+import { RangeDirective, ColumnsDirective, ColumnDirective } from '@syncfusion/ej2-react-spreadsheet';
 import Header from '../header/Header'
-import { debounce } from '../../utils/debounce';
+import { data } from './data';
 import './spreadsheet.css'
 
 function Spreadsheet() {
-    // use useState
-    let spreadsheet;
-
-    useEffect(() => {
-        fetch('http://localhost:8080/api/v1/sheet/get', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ Email: "user@gmail.com", Name: spreadsheet.sheets[0].name }),
-        })
-            .then((response) => {
-                if (response.status !== 204) {
-                    response.json().then((data) => {
-                        spreadsheet.openFromJson({ file: data.JSONData });
-                    })
-                }
-            })
-    }, [])
-
-
-    const saveSheetToDB = debounce(() => {
-        spreadsheet.endEdit();
-        spreadsheet.saveAsJson().then(Json => (fetch('http://localhost:8080/api/v1/sheet/update', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ Email: "user@gmail.com", Name: spreadsheet.sheets[0].name, JSONData: JSON.stringify(Json.jsonObject), ContentType: "Xlsx", VersionType: "Xlsx" }),
-        })))
-    }, 2000)
-
     return (
         <div className='spreadsheet'>
             <Header title="Spreadsheet" />
             <SpreadsheetComponent
-                ref={(scope) => { spreadsheet = scope }}
                 height="85%"
                 allowOpen={true}
                 openUrl='https://ej2services.syncfusion.com/production/web-services/api/spreadsheet/open'
                 allowSave={true}
-                saveUrl='https://ej2services.syncfusion.com/production/web-services/api/spreadsheet/save'
-                cellEditing={saveSheetToDB}
-            />
-        </div >
+                saveUrl='https://ej2services.syncfusion.com/production/web-services/api/spreadsheet/save'>
+                <SheetsDirective>
+                    <SheetDirective>
+                        <RangesDirective>
+                            <RangeDirective dataSource={data}></RangeDirective>
+                        </RangesDirective>
+                        <ColumnsDirective>
+                            {data.map((item, index) => <ColumnDirective key={index} width={150} />)}
+                        </ColumnsDirective>
+                    </SheetDirective>
+                </SheetsDirective>
+            </SpreadsheetComponent>
+        </div>
     )
 }
 
